583b4c23a00c2f2b3c247a7defdd6eec8bdf8ef3
daynot <daynortito13@gmail.com>
2024-07-14 23:00:48 -0400

    DeleteENV

--- Week1/crudGame/src/app.js
+++ Week1/crudGame/src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { sequelize } = require('./models'); // Asegúrate de que esta ruta sea correcta
-const gameRoutes = require('./routes/gameRoutes'); // Importa las rutas
+const { sequelize } = require('./models');
+const gameRoutes = require('./routes/gameRoutes');
 
 const app = express();
 app.use(express.json());
-app.use('/api', gameRoutes); // Configura las rutas
+app.use('/api', gameRoutes);
 
-// Sincroniza la base de datos
 sequelize.sync()
     .then(() => {
         console.log('Tables have been created');
